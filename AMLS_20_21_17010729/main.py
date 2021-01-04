8adf0a9d62c0311b325c06b508435eddfaee2c02
BigThighDude <72790316+BigThighDude@users.noreply.github.com>
2021-01-04 03:43:47 +0000

    Final Code
    Tuned hyperparameters for each model, as well as full commenting.

--- AMLS_20_21_17010729/main.py
+++ AMLS_20_21_17010729/main.py
@@ -8,45 +8,48 @@ import B2.B2 as by
 start = time.time()
 ########### Task A1: Gender Detection ###########
 # Arg1 - Create: 0, Train: 1, Test: 2
+# Arg3 - Hide Confusion Matrix: 0, Show Confusion Matrix: 1
 start1 = time.time()
-modelax = ax.main(0, None)
-trainax = ax.main(1, modelax)
-testax = ax.main(2, modelax)
+modelax = ax.main(0, None, None)
+trainax = ax.main(1, modelax, 0)
+testax = ax.main(2, modelax, 0)
 end1 = time.time()
 
 # ########### Task A2: Smile Detection ###########
 # Arg1 - Create: 0, Train: 1, Test: 2
+# Arg3 - Hide Confusion Matrix: 0, Show Confusion Matrix: 1
 start2 = time.time()
-modelay = ay.main(0, None)
-trainay = ay.main(1, modelay)
-testay = ay.main(2, modelay)
+modelay = ay.main(0, None, None)
+trainay = ay.main(1, modelay, 0)
+testay = ay.main(2, modelay, 0)
 end2 = time.time()
 
-# ########### Task B1: Face Shape Recognition ###########
+########### Task B1: Face Shape Recognition ###########
 # Arg1 - Create: 0, Train: 1, Test: 2
+# Arg3 - Hide Confusion Matrix: 0, Show Confusion Matrix: 1
 start3 = time.time()
-modelbx = bx.main(0, None)
-trainbx = bx.main(1, modelbx)
-testbx = bx.main(2, modelbx)
+modelbx = bx.main(0, None, None)
+trainbx = bx.main(1, modelbx, 0)
+testbx = bx.main(2, modelbx, 0)
 end3 = time.time()
 
 # ########### Task B2: Eye Colour Recognition ###########
 # Arg1 - Create: 0, Train: 1, Test: 2
+# Arg3 - Hide Confusion Matrix: 0, Show Confusion Matrix: 1
 start4 = time.time()
-modelby = by.main(0, None)
-trainby = by.main(1, modelby)
-testby = by.main(2, modelby)
+modelby = by.main(0, None, None)
+trainby = by.main(1, modelby, 0)
+testby = by.main(2, modelby, 0)
 end4 = time.time()
 
-print("Task A1 - Training accuracy:\t", str(trainax)[:4], "\nTask A1 - Unseen accuracy:\t\t", str(testax)[:4], "\nTask A2 - Training accuracy:\t", str(trainay)[:4], "\nTask A2 - Unseen accuracy:\t\t", str(testay)[:4], "\nTask B1 - Training accuracy:\t", str(trainbx)[:4],"\nTask B1 - Unseen accuracy:\t\t", str(testbx)[:4], "\nTask B2 - Training accuracy:\t", str(trainby)[:4],"\nTask B2 - Unseen accuracy:\t\t", str(testby)[:4])
+print("Task A1 - Training accuracy:\t", str(trainax)[:5], "\nTask A1 - Unseen accuracy:\t\t", str(testax)[:5], "\nTask A2 - Training accuracy:\t", str(trainay)[:5], "\nTask A2 - Unseen accuracy:\t\t", str(testay)[:5], "\nTask B1 - Training accuracy:\t", str(trainbx)[:5], "\nTask B1 - Unseen accuracy:\t\t", str(testbx)[:5], "\nTask B2 - Training accuracy:\t", str(trainby)[:5],"\nTask B2 - Unseen accuracy:\t\t", str(testby)[:5])
 
 end = time.time()
 print("Time for A1:\t\t", end1-start1)
 print("Time for A2:\t\t", end2-start2)
 print("Time for B1:\t\t", end3-start3)
 print("Time for B2:\t\t", end4-start4)
-print("Total time elapsed:\t",end-start)
+print("Total time elapsed:\t", end-start)
 
-# 3.5 seconds to run with pickle files
-# 260 seconds to run without pickle files
-
+# ~12 seconds to run with pickle files
+# 6 minutes to run without pickle files
